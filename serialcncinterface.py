7f5b5211be4d73e99a3dc51a5b26ecfdc5d91151
Henrique Hafner Ferreira <henriquehafner@poli.ufrj.br>
2021-03-06 17:23:03 -0300

    Refactoring.

--- serialcncinterface.py
+++ serialcncinterface.py
@@ -22,11 +22,12 @@ class SerialCNCInterface():
         #comand_file attributes
         self.gcode_status = 0
         self.gcode_custom_message = ['Null']
-        self.gcode_file = [['0'],5,'Null',0] # [Gcode File, file line pos,file name,file lines size]
-        self.gcode_files_dir = ['']
-        self.gcode_file_adress = os.path.join(os.getcwd(),'test.gcode')
-        self.gcode_files_list = [self.gcode_file_adress]
-        self.gcode_custom_message = ['G28 X Y','G1 F480 X101.084 Y92.419','G1 X101.379 Y92.547','G1 X103.682 Y92.844','G1 X104.345 Y92.969','G1 X104.989 Y93.173','G1 X105.604 Y93.45','G1 X106.182 Y93.799','G1 X106.557 Y94.08','G1 X107.164 Y94.287','G1 X107.958 Y94.685','G1 X109.303 Y95.497','G1 X109.387 Y95.524','G1 X110 Y95.805','G1 X110.576 Y96.157','G1 X111.011 Y96.5']
+        self.gcode_data = [['0'],5,'Null',0] # [Gcode File, file line pos,file name, end of file line index]
+        self.gcode_datas_dir = ['']
+        self.gcode_data_name = 'Null'
+        self.gcode_data_adress = 'Null'
+        self.gcode_datas_list = ['Null']
+        self.gcode_custom_message = ['G1 F480 X101.084 Y92.419','G1 X101.379 Y92.547','G1 X103.682 Y92.844','G1 X104.345 Y92.969','G1 X104.989 Y93.173','G1 X105.604 Y93.45','G1 X106.182 Y93.799','G1 X106.557 Y94.08','G1 X107.164 Y94.287','G1 X107.958 Y94.685','G1 X109.303 Y95.497','G1 X109.387 Y95.524','G1 X110 Y95.805','G1 X110.576 Y96.157','G1 X111.011 Y96.5','G1 F480 X101.084 Y92.419','G1 X101.379 Y92.547','G1 X103.682 Y92.844','G1 X104.345 Y92.969','G1 X104.989 Y93.173','G1 X105.604 Y93.45','G1 X106.182 Y93.799','G1 X106.557 Y94.08','G1 X107.164 Y94.287']
  
 
     def find_serial_device(self):
@@ -60,11 +61,11 @@ class SerialCNCInterface():
             rear = True
             stime = time.time()
             while rear == True:
-                message = self.ControllerCOM.read(char_limit)
+                message = self.ControllerCOM.read(len(feedback))
                 if debug == True: print(message)
                 if message == feedback:
                     rear = False
-                    return True
+                    return 'FB1'
                 elif len(message) > 0:
                     print(message)
                     rear = False
@@ -81,18 +82,22 @@ class SerialCNCInterface():
         message = message.decode('utf-8')
         return message
     
-    def write_cnc_serial(self,data):
-        if data[-1] != '\n':
-            data = data+'\n'
+    def write_cnc_serial(self,data,assure_ln=True):
+        if self.ControllerCOM == False:
+            print('Missing serial Device. ',data)
+            return False            
+        if assure_ln == True:          
+            if data[-1] != '\n':
+                data = data+'\n'
         data = bytes(data,'utf-8')
-        if self.ControllerCOM != False:
-            self.ControllerCOM.write(data)
-            feedback_answer = self.read_cnc_serial(feedback=bytes('ok\n','utf-8'),char_limit=3)
-            if feedback_answer != True:
-                print(feedback_answer)
+        self.ControllerCOM.write(data)
+        feedback_answer = self.read_cnc_serial(feedback=bytes('ok\n','utf-8'),char_limit=3)
+        if feedback_answer == 'FB1':
+            return 'FB1'
+        elif feedback_answer == True:
             return True
         else:
-            print('Missing serial Device. ',data)
+            print(feedback_answer)
             return False
 
     def CNCStatus(self):
@@ -109,47 +114,43 @@ class SerialCNCInterface():
         self.ControllerCOM.close()
         return True
 
-    def get_gcode_data(self,fileadress):       
-        with open(fileadress) as f:
-            gcode_data = f.read()
-        gcode_data = gcode_data.split('\n')
-        return gcode_data
-
-    def stream_gcode(self):
-        gc_line_pos = self.gcode_file[1]
-        gc_linebwrite = self.gcode_file[0][gc_line_pos]
-        self.gcode_file[1] +=1
-        if gc_linebwrite[0] == ';':
-            return 'Not valid command line'
-        self.write_cnc_serial(gc_linebwrite)
-        return True 
-    
-    def gcode_move_lastlinepos(self,line,operator='abs'):
-        curr_line = self.gcode_file[1]
-        target_line = None
-        if operator == 'abs':
-            target_line = line
-        elif operator == 'relative':
-            target_line = curr_line+line
-        if target_line < 0:
-            print('requested target_line was negative. Going to 0 instead')
-            target_line = max(0,target_line)
-        self.gcode_file[1] = target_line
+    def gcode_set_data(self):
+        with open(self.gcode_data_adress) as f:
+            gcode_data_l = f.read()
+        gcode_data_l = gcode_data_l.split('\n')
+        target_file_index = self.gcode_data_adress.rindex('\\') #finding star position of file name string.
+        gcode_data_name_l = self.gcode_data_adress[target_file_index+1:] #extracting the file_name from path
+        self.gcode_data_name = gcode_data_name_l
+        self.gcode_data[0] = gcode_data_l
+        self.gcode_data[1] = 0
+        self.gcode_data[2] = gcode_data_name_l
+        self.gcode_data[3] = len(gcode_data_l)-1 # subtractin 1 to start in line 0 index 0
         return True
-        
-    def gcode_file_handler(self,arg_def='test'):#fazer o ponteiro andar sobre os códigos
+
+    def gcode_data_handler(self,arg_def='test',gcode_datas_dir_l='None'):#pick a gcode file
+        if gcode_datas_dir_l == 'None':
+            gcode_datas_dir_l = os.getcwd()
+
         if arg_def == 'test':
-            self.gcode_files_dir = os.getcwd()
-            filename='test.gcode'
-        elif arg_def == 'GetNext' or arg_def == 'GetPrev':
-            self.gcode_files_list = glob.glob(os.path.join(self.gcode_files_dir,'*.gcode'))
-            self.gcode_files_list.sort(key=os.path.getctime,reverse=True)
-        elif type(arg_def) == list:
-            self.gcode_files_dir = arg_def[0]           
-            filename = arg_def[1]
+            gcode_data_name_l = 'test.gcode' # _l means a local scope var of instance propertie.
+            gcode_datas_list_l = glob.glob(os.path.join(gcode_datas_dir_l,'*.gcode'))
+        elif arg_def == 'GetNewest':
+            gcode_datas_list_l = glob.glob(os.path.join(gcode_datas_dir_l,'*.gcode'))
+            gcode_datas_list_l.sort(key=os.path.getctime,reverse=True)
+            target_file_index = gcode_datas_list_l[0].rindex('\\') #finding star position of file name string.
+            gcode_data_name_l = gcode_datas_list_l[0][target_file_index+1:] #extracting the file_name from path
+        elif type(arg_def) == list: #set a path in index 0 and file name in index 1
+            gcode_data_name_l = arg_def[1]
+            gcode_datas_dir_l = arg_def[0]           
+            gcode_datas_list_l=[os.path.join(gcode_datas_dir_l,gcode_data_name_l)]
         else:
+            print('Unexpected arg_def argument.')
             return False
-        self.gcode_file_adress = os.path.join(self.gcode_files_dir,filename)
+        
+        self.gcode_data_name = gcode_data_name_l
+        self.gcode_datas_dir = gcode_datas_dir_l
+        self.gcode_datas_list = gcode_datas_list_l
+        self.gcode_data_adress = os.path.join(gcode_datas_dir_l,gcode_data_name_l)
         return True
 
     def gcode_stream_handler(self): #flush not working!!!
@@ -163,13 +164,44 @@ class SerialCNCInterface():
         elif self.gcode_status == 1:
             return self.stream_gcode()
 
-        
+    def stream_gcode(self):
+        gc_line_pos = self.gcode_data[1]
+        gc_linebwrite = self.gcode_data[0][gc_line_pos]
+        self.gcode_data[1] +=1
+        if gc_linebwrite[0] == ';':
+            return 'Not valid command line'
+        self.write_cnc_serial(gc_linebwrite)
+        return True 
     
+    def gcode_move_lastlinepos(self,line,operator='abs'):
+        curr_line = self.gcode_data[1]
+        target_line = None
+        if operator == 'abs':
+            target_line = line
+        elif operator == 'relative':
+            target_line = curr_line+line
+        if target_line < 0:
+            print('requested target_line was negative. Going to 0 instead')
+            target_line = max(0,target_line)
+        self.gcode_data[1] = target_line
+        return True
+        
+    def gcode_command_inster(command='None'):
+        return
+
     def run(self):
         self.bind_communication()
-        self.gcode_file[0] = self.get_gcode_data(self.gcode_file_adress)
+        self.gcode_data_handler('GetNewest')
+        self.gcode_set_data()
 
 
+#quando recarregado, file_handler nao atualiza as propriedades gcode_data
+test = True
+if test == True:
+   tester = SerialCNCInterface()
+   tester.run()
+   #tester.write_cnc_serial('G28 X Y')
+   #tester.gcode_stream_handler()
     
 
 
