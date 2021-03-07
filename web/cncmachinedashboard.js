3d3ce5d869da35ea96a4599dc4337e9d4df2e30d
Henrique Hafner Ferreira <henriquehafner@poli.ufrj.br>
2021-03-07 11:59:12 -0300

    Dashboard Dynamics
    -Dashboards can be dinamically added now.

--- web/cncmachinedashboard.js
+++ web/cncmachinedashboard.js
@@ -20,32 +20,17 @@ function view_container_create(element_idtoset) {
     title_para.appendChild(title_text);
     div_to_add.appendChild(title_para);
     
+    script = document.createElement('script');
+    script.src = "script_".concat(element_idtoset.concat(".js"))
+    //script.src = "script_Gcode_Terminal.js";
+    console.log(script.src);
+    
     div_to_add.appendChild(canvas_toadd);
     div_main.appendChild(div_to_add);
+    
+    document.body.appendChild(script)
 }
 
-view_container_create("Gcode Terminal")
-var canvas = document.getElementById("canvas_Gcode Terminal"); //instancia controladora do Canvas
-var ctx = canvas.getContext("2d"); //instancia do operador grafico 2d
-ctx.beginPath();
-ctx.lineWidth = "2";
-ctx.strokeStyle = "white";
-ctx.rect(2, 105, 496, 18);
 
-async function update_client() {
-    let value = await eel.update_client()();
-	ctx.fillStyle = '#808080'; //define estilo do que vai ser desenhado
-	ctx.fillRect(0, 0, canvas.width, canvas.height); //Limpar area a ser desenhada
-	ctx.fillStyle = '#ffffff'; //cor do texto
-	ctx.font = '20px serif'; // formatting text
-	
-	var linecount = 1;
-	for (var i in value) {
-		ctx.fillText(value[i],10,linecount*20);
-		linecount ++
-    ctx.stroke();
-	}
-}
-var UpdateClientRoutine = setInterval(update_client, 5);
 
 
