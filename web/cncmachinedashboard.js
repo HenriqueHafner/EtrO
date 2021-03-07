a257f3126c9a7e2eb17031d01c1d98669178d4f7
Henrique Hafner Ferreira <henriquehafner@poli.ufrj.br>
2021-03-07 11:25:48 -0300

    GUI dev
    Added dynamic DIVs

--- web/cncmachinedashboard.js
+++ web/cncmachinedashboard.js
@@ -1,20 +1,31 @@
-var canvas_gcode_terminal = document.createElement("CANVAS");
-canvas_gcode_terminal.id = "canvasCNC"
-canvas_gcode_terminal.width = 500;
-canvas_gcode_terminal.height =230;
-document.body.appendChild(canvas_gcode_terminal)
+div_main = document.getElementById("machine_dashboard");
 
-function canvas_create(canvas_idtoset) {
+//var canvas_gcode_terminal = document.createElement("CANVAS");
+//canvas_gcode_terminal.id = "canvasCNC";
+//canvas_gcode_terminal.width = 500;
+//canvas_gcode_terminal.height =230;
+//div_main.appendChild(canvas_gcode_terminal);
+
+function view_container_create(element_idtoset) {
+    
     var canvas_toadd = document.createElement("CANVAS");
-    canvas_toadd.id = canvas_idtoset
+    canvas_toadd.id = "canvas_".concat(element_idtoset);
     canvas_toadd.width = 500;
-    canvas_toadd.height =240;
-    var ctx_canvas_toadd = canvas_toadd.getContext("2d");
-    ctx_canvas_toadd.fillText(canvas_idtoset,10,20);
-    document.body.appendChild(canvas_toadd)
+    canvas_toadd.height =230;
+    
+    div_to_add = document.createElement("DIV");
+    div_to_add.id = "div_".concat(element_idtoset);
+    title_para = document.createElement("p");
+    title_text = document.createTextNode(element_idtoset);   
+    title_para.appendChild(title_text);
+    div_to_add.appendChild(title_para);
+    
+    div_to_add.appendChild(canvas_toadd);
+    div_main.appendChild(div_to_add);
 }
 
-var canvas = document.getElementById("canvasCNC"); //instancia controladora do Canvas
+view_container_create("Gcode Terminal")
+var canvas = document.getElementById("canvas_Gcode Terminal"); //instancia controladora do Canvas
 var ctx = canvas.getContext("2d"); //instancia do operador grafico 2d
 ctx.beginPath();
 ctx.lineWidth = "2";
@@ -36,9 +47,5 @@ async function update_client() {
 	}
 }
 var UpdateClientRoutine = setInterval(update_client, 5);
-//clearInterval(UpdateClientRoutine)
-
-
-console.log('cncmachinedashboard.js running.');
 
 
